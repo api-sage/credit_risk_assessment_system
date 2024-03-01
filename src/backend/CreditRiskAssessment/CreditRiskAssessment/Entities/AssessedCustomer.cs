e39a14dd5bcfa191986d4b7007d3168386f1c075
Paul Afolabi <paulafolabi745@gmail.com>
2024-03-01 12:36:11 +0100

    created entities for sql db

---
+++ src/backend/CreditRiskAssessment/CreditRiskAssessment/Entities/AssessedCustomer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditRiskAssessment.Entities;
+
+public class AssessedCustomer
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime AssessedDate { get; set; }
+    public string Name { get; set; }
+    public string BVN { get; set; }
+    public int Age { get; set; }
+    public string Occupation { get; set; }
+    public decimal LoanAmount { get; set; }
+    public decimal AnnualIncome { get; set; }
+    public decimal MonthlyNetSalary { get; set; }
+    public decimal InterestRate { get; set; }
+    public int NumberOfLoan { get; set; }
+    public int NumberOfDelayedPayment { get; set; }
+    public decimal OutstandingDebt { get; set; }
+    public decimal DebtToIncomeRatio { get; set; }
+    public int MonthsOfCreditHistory { get; set; }
+    public decimal PaymentOfMinimumAmount { get; set; }
+    public decimal MonthlyInstallmentAmount { get; set; }
+    public decimal AmountInvestedMonthly { get; set; }
+    public decimal MonthlyBalance { get; set; }
+    public int PredictedCreditScore { get; set; }
+    public string CreditRating { get; set; }
+}
---
+++ src/backend/CreditRiskAssessment/CreditRiskAssessment/Entities/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditRiskAssessment.Entities;
+
+public class Customer
+{
+    public string Name { get; set; }
+    [Key]
+    public int BVN { get; set; }
+    public int Age { get; set; }
+    public string Occupation { get; set; }
+    public decimal LoanAmount { get; set; }
+    public decimal AnnualIncome { get; set; }
+    public decimal MonthlyNetSalary { get; set; }
+    public decimal InterestRate { get; set; }
+    public int NumberOfLoan { get; set; }
+    public int NumberOfDelayedPayment { get; set; }
+    public decimal OutstandingDebt { get; set; }
+    public int MonthsOfCreditHistory { get; set; }
+    public decimal PaymentOfMinimumAmount { get; set; }
+    public decimal MonthlyInstallmentAmount { get; set; }
+    public decimal AmountInvestedMonthly { get; set; }
+    public decimal MonthlyBalance { get; set; }
+}
