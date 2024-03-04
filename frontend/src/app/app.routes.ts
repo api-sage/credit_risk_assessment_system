0fec0eb2ae7e70e57241f0d43680c7472e6eb575
KachyBiggs <michaels_kachy@yahoo.com>
2024-03-04 09:22:20 +0100

    logic implementation of guauge

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { aboutPageComponent } from '../about-page/about-page.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 
  export const routes: Routes = [
-    // { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
     { path: '', component: landingPageComponent },
     { path: 'create-account', component: CreateAccountComponent },
     { path: 'login', component: LoginComponent },
