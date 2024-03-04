18fb4ac00cc3850c6495c387a686d9616768dc7b
KachyBiggs <michaels_kachy@yahoo.com>
2024-03-04 18:32:01 +0100

    backend updates

---
+++ frontend/src/app/creditscore/creditscore.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creditscore',
+  standalone: true,
+  imports: [],
+  templateUrl: './creditscore.component.html',
+  styleUrl: './creditscore.component.css'
+})
+export class CreditscoreComponent {
+
+}
