5be4205a0c0d912a1d7dd86611564c3ba56d32d5
KachyBiggs <michaels_kachy@yahoo.com>
2024-03-05 11:06:15 +0100

    API service integrated and running

--- frontend/src/api.service.ts
+++ frontend/src/api.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = '';
 
-  constructor(private http: HttpClient) {}
+  private apiUrl = 'https://localhost:7108';
+  this: any;
 
-  getCreditScore(bvn: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/credit-score/${bvn}`);
+  constructor(private http: HttpClient) { }
+
+  // Rename the method to match the endpoint name
+  assessCreditHistory(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/AssessCreditHistory`, data);
   }
+  
 
-  getCreditHistory(bvn: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/credit-history/${bvn}`);
+  // Rename the method to match the endpoint name
+  getAssessedCreditHistory(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/GetAssessedCreditHistory`);
   }
 }
