fda98ce663c5fc496d9166deb6dff0bf83b902e5
Paul Afolabi <paulafolabi745@gmail.com>
2024-03-06 09:57:27 +0100

    Data Transfer Object implementation

---
+++ src/backend/CreditRiskAssessment/CreditRiskAssessment/DTO/AssessedCustomerDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditRiskAssessment.DTO;
+
+public class AssessedCustomerDTO
+{
+    public int SN { get; set; }
+    public string AssessedDate { get; set; }
+    public string Name { get; set; }
+    public string BVN { get; set; }
+    public int Age { get; set; }
+    public string Occupation { get; set; }
+    public double LoanAmount { get; set; }
+    public double AnnualIncome { get; set; }
+    public double MonthlyNetSalary { get; set; }
+    public double InterestRate { get; set; }
+    public int NumberOfLoan { get; set; }
+    public int NumberOfDelayedPayment { get; set; }
+    public double OutstandingDebt { get; set; }
+    public float DebtToIncomeRatio { get; set; }
+    public int MonthsOfCreditHistory { get; set; }
+    public bool PaymentOfMinimumAmount { get; set; }
+    public double MonthlyInstallmentAmount { get; set; }
+    public double AmountInvestedMonthly { get; set; }
+    public double MonthlyBalance { get; set; }
+    public int PredictedCreditScore { get; set; }
+    public string CreditRating { get; set; }
+    
+}
--- src/backend/CreditRiskAssessment/CreditRiskAssessment/Interfaces/ICheckCreditWorthinessService.cs
+++ src/backend/CreditRiskAssessment/CreditRiskAssessment/Interfaces/ICheckCreditWorthinessService.cs
@@ -1,4 +1,5 @@
-﻿using CreditRiskAssessment.Entities;
+﻿using CreditRiskAssessment.DTO;
+using CreditRiskAssessment.Entities;
 using CreditRiskAssessment.Infrastructure.Commons;
 using CreditRiskAssessment.ML.Models;
 using CreditRiskAssessment.Models.Request;
@@ -14,5 +15,5 @@ namespace CreditRiskAssessment.Interfaces;
 public interface ICheckCreditWorthinessService
 {
     Task<ResponseResult<AssessRiskLevelResponse>> AssessRiskLevel(string request);
-    Task<ResponseResult<List<AssessedCustomer>>> GetAssessedCreditHistory(string request);
+    Task<ResponseResult<List<AssessedCustomerDTO>>> GetAssessedCreditHistory(string request);
 }
